c6c221d8defb3c8f4b0a41a67cf376827bace00e
SKoschnicke <s.koschnicke@gfxpro.com>
2011-09-09 15:24:03 +0200

    refactored into separate function

--- main.hs
+++ main.hs
@@ -1,14 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Debug
 import Blaze.ByteString.Builder (fromByteString)
 import Network.HTTP.Types (status200)
 import qualified Data.ByteString.Char8 as B
 
-main = run 3000 $ const $ return $ ResponseBuilder
-    status200
-    [("Content-Type", "text/plain"), ("Content-Length", B.pack(show(B.length message)))]
-    (fromByteString message)
-  where
-    message :: B.ByteString
-    message = "Hello World"
+import System.Log.Logger
+import System.Log.Handler.Simple
+
+
+my_app :: Application -- type Application = Request -> Iteratee ByteString IO Response
+my_app request =
+    return $ ResponseBuilder -- Constructor for data Response: ResponseBuilder Status ResponseHeaders Builder
+      status200
+      [("Content-Type", "text/plain"), ("Content-Length", B.pack(show(B.length message)))]
+      (fromByteString message)
+    where
+      message :: B.ByteString
+      message = "Hello World"
+
+main = run 3000 $ debug my_app
