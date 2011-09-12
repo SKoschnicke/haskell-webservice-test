35c66a54bd22bb8213e1ad27e0d4c0eb9dcf7c17
Sven Koschnicke <s.koschnicke@gfxpro.com>
2011-09-12 21:03:18 +0200

    sending many chars

--- main.hs
+++ main.hs
@@ -2,7 +2,9 @@
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.Debug
-import Blaze.ByteString.Builder (fromByteString)
+import Blaze.ByteString.Builder
+import Blaze.ByteString.Builder.Char.Utf8 (fromChar)
+import Data.Monoid (mconcat)
 import Network.HTTP.Types (status200)
 import qualified Data.ByteString.Char8 as B
 
@@ -14,8 +16,9 @@ my_app :: Application -- type Application = Request -> Iteratee ByteString IO Re
 my_app request =
     return $ ResponseBuilder -- Constructor for data Response: ResponseBuilder Status ResponseHeaders Builder
       status200
-      [("Content-Type", "text/plain"), ("Content-Length", B.pack(show(B.length message)))]
-      (fromByteString message)
+      [("Content-Type", "text/plain")]
+      --(fromByteString $ mconcat (fromChar (replicate 4000 "a")))
+      (fromByteString $ B.replicate 4000000 'a')
     where
       message :: B.ByteString
       message = "Hello World"
